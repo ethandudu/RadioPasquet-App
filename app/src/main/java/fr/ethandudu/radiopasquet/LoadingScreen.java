ba017ff2880237ac4c3e60ea831393733a84b5d5
Ethan <ethandudutech@gmail.com>
2021-10-23 15:48:48 +0200

    Notif + Loading

--- app/src/main/java/fr/ethandudu/radiopasquet/LoadingScreen.java
+++ app/src/main/java/fr/ethandudu/radiopasquet/LoadingScreen.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.view.Window;
+import android.view.WindowManager;
+
 public class LoadingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_loading_screen);
 
         Runnable runnable = new Runnable() {
