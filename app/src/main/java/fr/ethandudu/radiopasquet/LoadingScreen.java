c2ab111d17747f5da3132ba5f5b19b9178cc5958
Ethan <ethandudutech@gmail.com>
2021-11-07 21:14:57 +0100

    Loading screen + Main
    Loading screen avec check économie de batterie + main avec webview chrome

--- app/src/main/java/fr/ethandudu/radiopasquet/LoadingScreen.java
+++ app/src/main/java/fr/ethandudu/radiopasquet/LoadingScreen.java
@@ -3,11 +3,16 @@ package fr.ethandudu.radiopasquet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class LoadingScreen extends AppCompatActivity {
 
@@ -28,5 +33,17 @@ public class LoadingScreen extends AppCompatActivity {
         };
         //Wait 3s
         new Handler().postDelayed(runnable, 3000);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                Toast.makeText(getApplicationContext(),"⚠ Merci de désactiver l'optimisation de la batterie", Toast.LENGTH_LONG).show();
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
     }
 }
